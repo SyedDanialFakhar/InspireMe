3eb04bfda50723bcefdc77987fdbc203f6d628d4
Syed Danial Fakhar <101790589+SyedDanialFakhar@users.noreply.github.com>
2026-01-19 22:20:27 +0500

    updated bugs
    Added a comment indicating the purpose of the app.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,6 +4,8 @@ import 'providers/theme_provider.dart';
 import 'router/app_router.dart';
 import 'theme/app_theme.dart';
 
+//inspire me app
+
 class InspireMeApp extends StatelessWidget {
   const InspireMeApp({super.key});
 
